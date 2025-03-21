a4b70c3fc41e20ccdd7bf17039551a29079a36d8
unknown <andrebeckpires@gmail.com>
2025-03-20 21:37:21 -0300

    seleçao de login

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import json
 
-#variaveis
+#variables
 userInput = ''
 passwordInput = ''
 autenticado = 0
-
+success = 0
 with open('credentials.json', 'r', encoding='utf-8') as arquivo:
-    users = json.load(arquivo)  # salva o json em uma variavel
+    users = json.load(arquivo)  # loads json file into a variable
+
+
+def login():
+    getUserInput()
+    autenticado = autenticate(userInput,passwordInput)
+    if autenticado == 0:
+        login()
+
+def createUser():
+    print("criar usuario")
 
 def getUserInput():
     global userInput
@@ -18,7 +28,8 @@ def autenticate(userInput, passwordInput):
     for user in users:
         if user['user'] == userInput:
             if user['password'] == passwordInput:
-                print("Conectado")
+                global success
+                success = 1
                 return 1
             else:
                 print("Senha incorreta")
@@ -26,7 +37,22 @@ def autenticate(userInput, passwordInput):
     print("Usuario nao encontrado")
     return 0
 
+def selectAction():
+    global success
+    print("Digite 1 para fazer login ou 0 para criar um novo usuario:")
+    action = input()
+    if action == '1':
+        login()
+    if action == '0':
+        createUser()
+    if success == 1:
+        print("Conectado")
+    else:
+        selectAction()
 
-while autenticado == 0:
-    getUserInput()
-    autenticado = autenticate(userInput,passwordInput)
+
+
+
+selectAction()
+
+
