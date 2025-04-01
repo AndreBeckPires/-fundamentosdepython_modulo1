03939c7a5ca5d4ebf2e9772cb896db50f28671b4
Beck Reichert <beckreichert@192.168.1.57>
2025-04-01 17:11:24 -0300

    logout function

--- app.py
+++ app.py
@@ -2,4 +2,16 @@ import manageusers
 
 while manageusers.success != 1:
     manageusers.selectAction()
+
+#loop principal
+
+while manageusers.getAutenticado() == 1:
+    print(f'Conectado como {manageusers.getUser()}. Digite 1 para fazer logout ou 2 para encerrar aplicação: ')
+    action = input()
+    if action == '1':
+        manageusers.logout()
+        manageusers.selectAction()
+    if action == '2':
+        exit()
+    
     
--- manageusers.py
+++ manageusers.py
@@ -6,6 +6,7 @@ passwordInput = ''
 autenticado = 0
 success = 0
 lista_de_usuarios = []
+currentUser = ''
 with open('credentials.json', 'r', encoding='utf-8') as arquivo:
     users = json.load(arquivo)  # loads json file into a variable
 
@@ -69,6 +70,8 @@ def autenticate(userInput, passwordInput):
     for user in users:
         if user['user'] == userInput:
             if user['password'] == passwordInput:
+                global currentUser
+                currentUser = user['user']
                 global success
                 success = 1
                 return 1
@@ -80,17 +83,31 @@ def autenticate(userInput, passwordInput):
 
 def selectAction():
     global success
-    print("Digite 1 para fazer login ou 0 para criar um novo usuario:")
+    print("Digite 1 para fazer login, 0 para criar um novo usuario ou 2 para encerrar aplicação:")
     action = input()
     if action == '1':
         login()
     if action == '0':
         createUser()
+    if action == '2':
+        exit()
     if success == 1:
         print("Conectado")
+
     else:
         selectAction()
 
+def getAutenticado():
+    return success
+
+
+def logout():
+    global success
+    success = 0
+    return success
+
+def getUser():
+    return currentUser
 
 
 
